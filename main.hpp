991b8907f0872dcc423c9290cb7f0e876334848c
Michael Hodges <156959963+Michael0199@users.noreply.github.com>
2024-03-12 23:10:42 +0000

    mixed

--- main.hpp
+++ main.hpp
@@ -1,5 +1,3 @@
-//
-
 #include <iomanip>
 #include <iostream>
 #include <cstdlib>
@@ -10,16 +8,32 @@ using namespace std;
 int getRandom(void);
 int getClosestMed(int, int, int);
 
-// complete two functions
 int getRandom(void)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+ int n=rand() % 99;
+  return n;
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-        /***************************************************
-         * Code your program here
-         ***************************************************/
+  int gerard=(rdnum1 + rdnum2 + rdnum3) / 3.0;
+  int r1,r2,r3,min;
+  if (gerard>rdnum1)
+    r1= gerard - rdnum1;
+    else 
+     r1= rdnum1 - gerard;
+  if (gerard>rdnum2)
+    r2= gerard - rdnum2;
+    else 
+     r2= rdnum2 - gerard;
+  if (gerard>rdnum3)
+    r3= gerard - rdnum3;
+    else 
+     r3= rdnum3 - gerard;  
+if (r1 <r2)
+    if (r1 <r3)
+      return rdnum1;
+    else return rdnum3;
+  else if (r2 <r3)
+    return rdnum2;
+  else return rdnum3;
 }
